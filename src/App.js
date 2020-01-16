34cd72b71fde2883b057200779c9281d62240797
Brendan Meehan <bmmeehan3@gmail.com>
2020-01-15 23:10:00 -0500

    initial working landing page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {LandingCanvas} from './CustomCanvas';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      styleIn: "transform: rotate(90deg); transition: ease-in .2s;",
+      styleOut: "transform: rotate(0deg); transition: ease-in .2s;"
+    }
+  }
+  scrollToProjects = () => {
+    let element = document.getElementById("blah");
+
+    element.scrollIntoView({block: 'start', behavior: 'smooth'});
+  }
+
+  mouseIn() {
+    let element = document.getElementById("arrow_rotate");
+    element.style = this.state.styleIn;
+  }
+  mouseOut() {
+    let element = document.getElementById("arrow_rotate");
+    element.style = this.state.styleOut;
+  }
+  render() {
+    return (
+      <div className="canvas_container">
+        <section className="name_title">
+          <p className="name">Brendan Meehan</p>
+          <p className="title">Full Stack Web Developer</p>
+        </section>
+        <section className="to_projects" onClick={() => this.scrollToProjects()} onMouseEnter={() => this.mouseIn()} onMouseLeave={() => this.mouseOut()}><p>Projects</p><i className="material-icons arrow_rotate" id="arrow_rotate">arrow_forward_ios</i></section>
+        <LandingCanvas name="Custom_Galaxy" background="#161416"/>
+        <section className="blah" id="blah">Blah blah blah</section>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/CustomCanvas.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Pt, Group, Line, Create, Circle} from 'pts';
+import {PtsCanvas} from 'react-pts-canvas';
+
+export class LandingCanvas extends PtsCanvas {
+
+    constructor() {
+        super();
+        this.pts = new Group();
+    }
+
+    _create() {
+        this.pts = Create.distributeRandom(this.space.innerBound, 175);
+    }
+    animate (time, ftime, space) {
+        this.pts.rotate2D(0.0004, space.center);
+        this.pts.sort( (a,b) => 
+        a.$subtract(this.space.pointer).magnitudeSq() - b.$subtract(this.space.pointer).magnitudeSq()
+        );
+        this.pts.forEach( (p, i) => {
+                let ratio = Math.min( 1, 1 - (this.space.pointer).$subtract(p).magnitude()/(this.space.size.x / 8) );
+
+            
+                let c = Circle.fromCenter(p, 1);
+                let c2 = Circle.fromCenter(p, 4);
+                if (ratio > .7) {
+                    this.form.stroke(`rgba(255,255,255, ${1 - ratio})`).line( [p, this.pts[i + 1]]);
+                    this.form.stroke(`rgba(255,255,255, ${1 - ratio})`).line( [this.pts[i + 1], this.pts[i + 2]]);
+                    this.form.stroke(`rgba(255,255,255, ${1 - ratio})`).line( [this.pts[i+2], this.pts[i + 3]]);
+
+                }
+                this.form.fillOnly(`rgba(255,255,255,${1 + ratio})`, ratio * 2).circle(c);
+                this.form.fill(`rgba(255,255,255,${ratio})`).circle(c2);
+                //form.stroke("rgba(255,255,255, .2").line([c])
+            })
+        }
+
+    start (bound, space) {
+       this._create();
+    }
+
+    action (type, x, y, event) {
+
+    }
+
+    resize (size, event) {
+        this._create();
+    }
+}
+
